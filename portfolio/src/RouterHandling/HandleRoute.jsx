fc2804475b774a896443eb82ab966475c93efec3
Aryan <Aryanchaudhary2629@gmail.com>
2025-07-18 19:16:57 +0530

    Fixing issue

--- portfolio/src/RouterHandling/HandleRoute.jsx
+++ portfolio/src/RouterHandling/HandleRoute.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import {Routes, Route} from 'react-router-dom'
-import Home from '../Pages/home'  
+import Home from '../Pages/Home'  
 import About from '../Pages/About'
 import Projects from '../Pages/Project'
 import Skills from '../Pages/Skills'
